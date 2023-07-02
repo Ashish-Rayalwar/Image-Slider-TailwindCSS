b1de6e0d574eae6641ef0bf507effa70396528d5
Ashish Rayalwar <rayalwar.ashish.1@gmail.com>
2023-07-02 12:33:17 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import Caraousel from "./Caraousel";
+
+function App() {
+  return <Caraousel />;
+}
+
+export default App;
---
+++ src/images.jsx
@@ -0,0 +1,11 @@
+export const slides = [
+  {
+    url: "https://media.zigcdn.com/media/model/2023/Mar/rear-3-4-left-53502271_600x400.jpg",
+  },
+  {
+    url: "https://editorial.pxcrush.net/carsales/general/editorial/lamborghini_bruce_pi_full_res-14.jpg?width=1024&height=683",
+  },
+  {
+    url: "https://elmenyautozas.eu/wp-content/uploads/2023/01/lambo_h.jpeg",
+  },
+];
